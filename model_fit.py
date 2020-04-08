550e4a79eed06916eaf3f2fcf9b44052bd4c5a45
Jayanti Prasad <prasad.jayanti@gmail.com>
2020-04-08 18:12:09 +0530

    Data fitting updates

--- model_fit.py
+++ model_fit.py
@@ -6,84 +6,82 @@ from scipy.optimize import curve_fit
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import argparse
+import datetime
+
+now = datetime.datetime.now()
+now = now.strftime("%Y-%m-%d %H:%M:%S")
 
 
-def func(x, a, b, c):
-   return a * np.exp(-b * x)  
+def func(x, a, b):
+
+   return a * np.exp(-b * x)   
 
 if __name__ == "__main__":
 
    parser = argparse.ArgumentParser()
    parser.add_argument('-i', '--input-file', help='Input cvv file')
-   parser.add_argument('-s', '--start-day', help='Star day for fitting data',type=int)
-   parser.add_argument('-e', '--end-day', help='End day for fitting data', type=int)
-   parser.add_argument('-o', '--output-dir', help='Output dir')
+   parser.add_argument('-s', '--start-day', help='Star day for fitting data',type=int,default=10)
+   parser.add_argument('-e', '--end-day', help='End day for fitting data', type=int,default=20)
+   parser.add_argument('-o', '--output-dir', help='Output dir',default="plots")
    args = parser.parse_args()
 
    os.makedirs(args.output_dir, exist_ok=True)
 
    df = pd.read_csv(args.input_file)
+
+   for index, rows in df.iterrows():
+      print(index, rows['date'], rows['confirmed'],rows['recovered'],rows['deaths'])
+
+
    start = args.start_day
    end = args.end_day 
 
-   df_in = df[df['Country/Region'] == 'India']
+   dates = df['date'].to_list() 
+   dates = [str(d).replace("-2020","") for d in dates] 
 
-   dates = df_in['ObservationDate'].to_list()
-   dates = [x.replace('/2020','') for x in dates]
+   days = np.array([float(i) for i in range(0, len(dates))])
 
-   deaths = df_in['Deaths'].to_list()    
-   confirmed = df_in['Confirmed'].to_list()    
+   cases=["deaths","recovered","confirmed"]
 
-   fit_deaths = deaths[start:end]
-   fit_confirmed = confirmed[start:end]
-   fit_dates = dates[start:end]
+   yy = [] 
+   for case in cases:
+      tt = df[case].to_list()
+      yy.append(tt) 
+ 
 
-   full_dates = dates[start:] 
-   full_deaths= deaths[start:]
-   full_confirmed = confirmed[start:]
+   fig = plt.figure(figsize=(18,18))
 
-   full_days  = np.array([i for i in range(0, len(full_deaths))])
+   ax = [] 
+   ax.append(plt.subplot(311))
+   ax.append(plt.subplot(312))
+   ax.append(plt.subplot(313))
 
-   xdata = np.array([ i for i in range(0, len(fit_dates))])
+   for i in range(0, len(cases)):
+      ax[i].plot(dates, yy[i], 'bo',label='Full data' )
+      plt.setp(ax[i].get_xticklabels(), rotation=90, horizontalalignment='right')
+      ax[i].set_ylabel(cases[i])
+      ax[i].grid()
+      if i < 2 :
+         ax[i].set_xticklabels([])
+      if i == 0: 
+         ax[i].set_title("Covid-19 data fitting [a*exp(-b*x)] for India, created@: " + now)
+         
+      start = 0 
+      for j in range(0, len(yy[i])):
+         if start == 0 and yy[i][j] > 0:
+            start = j
 
-   fig = plt.figure(figsize=(12,6))
+      x_fit = np.array(days[start:end]-days[start])
+      dates_fit = np.array(dates[start:end])
+      y_fit = np.array(yy[i][start:end])
+ 
+      ax[i].plot(dates_fit, y_fit, 'ro',label='Fitting data' )
+      popt, pcov = curve_fit(func, x_fit, y_fit)
+      print("coeff:",popt) 
 
-   ax1 = plt.subplot(121)
-   ax2 = plt.subplot(122)
-
-   ydata = np.array([float(x) for x in  fit_deaths]) 
-
-   ax1.plot(full_dates, full_deaths, 'bo',label='Full data' )
-   ax1.plot(fit_dates, fit_deaths, 'ro',label='Fitted data')
-    
-   popt, pcov = curve_fit(func, xdata, ydata)
-
-   print("params:",popt[0],popt[1],popt[1])
-
-   ax1.plot(full_days, func(full_days, *popt), 'k-',\
-     label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt))
-   #ax1.set_xticks(full_days, full_dates, rotation=90) 
-   plt.setp(ax1.get_xticklabels(), rotation=90, horizontalalignment='right')
-   ax1.set_title("Number of deaths")
-   ax1.grid()
-   ax1.legend()
-    
-
-   ydata = np.array([float(x) for x in  fit_confirmed])
-
-   ax2.plot(full_dates, full_confirmed, 'bo',label='Full data' )
-   ax2.plot(fit_dates, fit_confirmed, 'ro',label='Fitted data')
-
-   popt, pcov = curve_fit(func, xdata, ydata)
-
-   print("params:",popt[0],popt[1],popt[1])
-
-   ax2.plot(full_days, func(full_days, *popt), 'k-',\
-     label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt))
-   #ax1.set_xticks(full_days, full_dates, rotation=90) 
-   plt.setp(ax2.get_xticklabels(), rotation=90, horizontalalignment='right')
-   ax2.set_title("Number of Confimed cases")
-   ax2.grid()
-   ax2.legend()
-   plt.savefig(args.output_dir + os.sep + "india_"+str(start)+"_"+str(end)+"_fit.pdf")
+      ax[i].plot(dates[start:], func(days[start:]-days[start], *popt), 'k-',\
+         label='fit: a=%5.3f, b=%5.3f' % tuple(popt))
+      ax[i].legend()
+  
+   plt.savefig(args.output_dir + os.sep + "exp_model_fit.pdf")
 
--- plot_cdr.py
+++ plot_cdr.py
@@ -5,6 +5,10 @@ import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 import argparse 
+import matplotlib
+
+plt.rcParams.update({'font.size': 22})
+
 
 """
 Plot the number of confirmed (c), deaths (d) and recovered (r)
@@ -20,8 +24,11 @@ if __name__ == "__main__":
    parser.add_argument('-','--input-file',help='Input CSV file',\
       default='data/full-data-07-04-2020.csv')
    parser.add_argument('-c','--country-name',help='Country name',default='India')   
-   parser.add_argument('-n','--num-days',help='Number of days to plot',\
+   parser.add_argument('-s','--start-day',help='Starting day for  plot',\
       type=int,default=30)
+   parser.add_argument('-e','--end-day',help='End day for  plot',\
+      type=int,default=400)
+
    parser.add_argument('-o','--output-dir',help='Output dir',default='plots')
 
    args = parser.parse_args()
@@ -31,7 +38,8 @@ if __name__ == "__main__":
    df = pd.read_csv(args.input_file)
    df = df[df['country'] == args.country_name]
   
-   n = args.num_days 
+   start = args.start_day 
+   end = args.end_day 
 
    print(df.shape, df.columns)
    df = df.fillna(0)
@@ -46,7 +54,9 @@ if __name__ == "__main__":
    dates2 = dates1.sort()
 
    x, yc, yd, yr = [], [], [], []  
-   count = 0   
+
+   dfc = pd.DataFrame(columns=['date','confirmed','deaths','recovered'])
+   count = 0 
    for d in dates1:
      dd = str(d).split()[0].split("-")
      key = dd[1]+"-"+dd[2]+"-"+dd[0]
@@ -55,31 +65,37 @@ if __name__ == "__main__":
      yc.append (D[key][0])
      yd.append (D[key][1])
      yr.append (D[key][2])
+     dfc.loc[count] = [key,D[key][0], D[key][1], D[key][2]]
      count +=1
+
+   dfc.to_csv(args.output_dir + os.sep + "cdr_" + args.country_name +".csv")
 
    dates1 = [str(x).replace('2020-','') for x in dates1]
    dates = [x.split(" ")[0] for x in dates1]
 
-   fig  = plt.figure(figsize=(12,12))
+   fig  = plt.figure(figsize=(18,18))
 
    ax = fig.add_subplot(311)
-   ax.set_title("Deaths")
+   ax.set_title("Covid-19: "+args.country_name)
    plt.grid()
+   ax.set_ylabel("Deaths")
    ax.set_xticklabels([])
 
-
    bx = fig.add_subplot(312)
-   bx.set_title("Recovered")
+   bx.set_ylabel("Recovered")
    bx.set_xticklabels([])
    plt.grid()
 
    cx = fig.add_subplot(313)
    plt.xticks(rotation=90)
-   cx.set_title("Infected")
+   cx.set_ylabel("Infected")
+   cx.tick_params(axis='x', which='major', labelsize=10)
+   cx.tick_params(axis='x', which='minor', labelsize=8)
+
      
-   ax.plot(dates[-n:], yd[-n:])
-   bx.plot(dates[-n:], yr[-n:])
-   cx.plot(dates[-n:], yc[-n:])
+   ax.plot(dates[start:end], yd[start:end],'r',linewidth=4.0)
+   bx.plot(dates[start:end], yr[start:end],'g',linewidth=4.0)
+   cx.plot(dates[start:end], yc[start:end],'b',linewidth=4.0)
    plt.grid()
    plt.savefig(args.output_dir + os.sep + "cdr_" + args.country_name +".pdf")
 
