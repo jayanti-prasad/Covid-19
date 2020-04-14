8c7457f7688254d707509fb7c883156eb5da1766
Jayanti Prasad <prasad.jayanti@gmail.com>
2020-04-14 19:32:44 +0530

    Plots updated

--- reconstruction/plot_multi.py
+++ reconstruction/plot_multi.py
@@ -4,9 +4,11 @@ import sys
 import os
 import glob 
 import matplotlib 
+import numpy as np
 
-matplotlib.rc('xtick', labelsize=16) 
-matplotlib.rc('ytick', labelsize=16) 
+fontsize = 18 
+matplotlib.rc('xtick', labelsize=fontsize) 
+matplotlib.rc('ytick', labelsize=fontsize) 
 matplotlib.rcParams['axes.linewidth'] = 1.0 
 
 
@@ -19,21 +21,29 @@ if __name__ == "__main__":
     ax.set_xlim(0,45)
     ax.set_xlabel('Number of days since '+r'$ t_i$',fontsize=16)
     if sys.argv[2] == 'beta':
-       ax.set_ylabel('raw '+ r'$\beta$(t)',fontsize=16)
+       ax.set_ylabel('raw '+ r'$\beta$(t)',fontsize=fontsize)
     else: 
-      ax.set_ylabel(sys.argv[2],fontsize=16) 
+      ax.set_ylabel(sys.argv[2],fontsize=fontsize) 
+   
+    
+    colors=['r','b','g','k']
  
-    for f in files[:6]:
+    count = 0
+    for f in [files[2], files[0], files[1]]:
        y = pd.read_csv(f)[sys.argv[2]].to_numpy()
+       x = [float(i) for i in range(0, y.shape[0])]
+       x = np.array(x)
        lab = f.replace(sys.argv[1]+"Italy_",'')
        lab = lab.replace(".csv",'')
        parts = lab.split("_")
        print("parts:",parts)
-       label = r'$1/\sigma=$'+parts[3]+r', $1/\gamma=$'+parts[6]
+       label = r'$1/\sigma=$'+parts[3] 
+       #label = r'$1/\gamma=$'+parts[6]
        label = label.replace('0','').replace('.','')   
-       ax.plot(y,label=label, lw='2')
+       ax.plot(x,y,label=label,c=colors[count], lw='2')
+       ax.scatter(x,y,c=colors[count])
        ax.legend(fontsize='large')
-
+       count +=1
 
     plt.savefig(sys.argv[1] + os.sep + sys.argv[2]+".pdf")
     plt.show()
