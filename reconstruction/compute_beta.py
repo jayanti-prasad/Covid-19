6b033c3296596db22fd0b994c9e2a94fd8880ddd
Jayanti Prasad <prasad.jayanti@gmail.com>
2020-04-19 14:12:46 +0530

    Beta plot updated

--- reconstruction/compute_beta.py
+++ reconstruction/compute_beta.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import argparse
 from common_utils import date_normalize, strip_year 
 import matplotlib
+from reconstruction import Reconstruct
 
 fontsize = 22
 matplotlib.rc('xtick', labelsize=fontsize)
@@ -14,16 +15,17 @@ matplotlib.rc('ytick', labelsize=fontsize)
 matplotlib.rcParams['axes.linewidth'] = 2.0
 matplotlib.rcParams["font.family"] = "Courier New"
 
-
-
 if __name__ == "__main__":
 
    parser = argparse.ArgumentParser()
    parser.add_argument('-i','--input-file',help='Input csv file',\
       default='../data/covid-19-global.csv')
    parser.add_argument('-c','--country-name',help='Country name')
-   parser.add_argument('-s','--start-date',help='Start data',type=int, default=0)
+   parser.add_argument('-s','--start-date',help='Start date',type=int, default=0)
    parser.add_argument('-o','--output-dir',help='Output dir',default="results")
+   parser.add_argument('-l','--lockdown-file',help='Lockdown file',\
+      default='../data/covid-19-lockdown.csv')
+
 
    args = parser.parse_args()
 
@@ -37,6 +39,8 @@ if __name__ == "__main__":
 
    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
    df = df.loc[:, ~df.columns.str.contains('country')]
+
+   df = df[df['confirmed'] > 25]
    
    X = df['confirmed'].to_numpy()  
    Y = df['deaths'].to_numpy()  
@@ -51,21 +55,35 @@ if __name__ == "__main__":
      beta[i] = ((I[i+1]-I[i]) + (R[i+1]-R[i]))/ I[i]
 
 
-   t = dates[args.start_date:-2]
-   x = beta[args.start_date:-1]
+   #t = dates[args.start_date:-2]
+   #x = beta[args.start_date:-1]
+
+   t = dates[:-1]
+
+   R = Reconstruct(args, args.country_name)
+
+   g, s, a = 7.0, 7.0, 1.0
+   R.solve (g, s, a)
+   beta1 = R.beta[:-2]  
+
+   #print("beta:",beta.shape)
+   #print("beta1:",beta1.shape)
 
    fig = plt.figure(figsize=(18,12))
    ax = fig.add_subplot()  
 
-   ax.plot(t, x,'o')
-   plt.setp(ax.get_xticklabels(), rotation=90, horizontalalignment='right',fontsize=10)
-   ax.axhline(y=0,c='k',ls='--')
-   ax.plot(t, x)
+   ax.plot(t, beta,'o',c='b')
+   ax.plot(t, beta,label='Direct',c='b')
+   ax.plot(t[:-1], beta1,label='Reconstructed',c='r')
+   ax.plot(t[:-1], beta1,'o',c='r')
    ax.set_xlabel('t')
    ax.set_ylabel(r'$\beta (t)$')
    ax.set_title(args.country_name)
+   plt.legend()
+   ax.axhline(y=0,c='k',ls='--')
+   plt.setp(ax.get_xticklabels(), rotation=90, horizontalalignment='right',fontsize=10)
    plt.savefig(args.output_dir + os.sep + "beta_" + args.country_name + ".pdf")
- 
+   plt.show() 
 
 
 
