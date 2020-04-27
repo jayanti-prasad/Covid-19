1e9879f4649e0bc8dd3da878b7b848b098fd9a42
Jayanti Prasad <prasad.jayanti@gmail.com>
2020-04-27 07:50:30 +0530

    Bugs in fitting  fixed

--- models/optimizer.py
+++ models/optimizer.py
@@ -6,43 +6,59 @@ import pandas as pd
 from scipy.optimize import minimize
 from datetime import timedelta, datetime
 import matplotlib.pyplot as plt
-from common_utils import get_country_data  
+from common_utils import get_country_data, strip_year  
 from epidemiology import SIR, SEIR, SEIARD, Epidemology 
+
+
+def get_fitting_data (args, df, country, start_date='2020-01-01'):
+   df  = get_country_data (df, country)
+   dates = df['date'].to_list()
+   data = df['confirmed'] - df['recovered'] - df['deaths']
+   data.index = df['date'].to_list()
+   return data[start_date:]
+
+def get_population(args, country):
+   df_p = pd.read_csv(args.population_file)
+   P = df_p['pop_2020'].str.replace(",","").astype(int)
+   P.index = df_p['country'].to_list()
+   return  P[country]
+
+
+def plot_data(data):
+   data.index = strip_year(data.index)
+   fig = plt.figure(figsize=(18,12))
+   ax = fig.add_subplot(111)
+   plt.plot(data)
+   plt.setp(ax.get_xticklabels(), rotation=90, horizontalalignment='right')
+   ax.axhline(y=25,c='k',ls='--')
+   plt.show()
 
 
 class Learner(object):
     """
     This is the main optimier class
     """
-    def __init__(self,args, N, df):
+    def __init__(self, N, data, model):
 
-        self.args = args
-        self.N = N 
+        self.model = model
+        self.data = data 
 
-        df  = get_country_data (df, args.country_name)
-        df = df[df['confirmed'] > 25]
- 
-        dates = df['date'].to_list() 
-        self.data = df['confirmed'] - df['recovered'] - df['deaths']
-        self.data.index = dates  
-        self.days = [int(i) for i in range(0, len(dates))]
+        I0, R0 = self.data[0], 0  
+        S0 =  N - I0 - R0 
 
-        self.I0, self.R0 = self.data[0], 0  
-        self.S0 =  self.N - self.I0 - self.R0 
-
-        self.E = Epidemology (args.model_name,'solve_ivp',  df.shape[0])
-        self.E.set_init (self.N, self.I0, self.R0)
-
+        self.E = Epidemology (self.model,'solve_ivp',  data.shape[0])
+        self.E.set_init (N, I0, R0)
+        print(N, I0, R0, S0)
 
     def initial_guess (self):
 
-        if self.args.model_name == 'sir':
+        if self.model == 'sir':
            starting_point =  [0.12, 0.11]
            bounds = [(1.0E-03, 10.0), (1.0E-03, 10.0)]
 
-        if self.args.model_name == 'dbsir':
+        if self.model == 'dbsir':
             starting_point = [0.12, 0.01, 0.11]
-            bounds = [(1.0E-08, 10.0), (1.0E-4,1.0), (1.0E-08, 10.0)]             
+            bounds = [(1.0E-04, 1.5), (1.0E-4,1.0), (1.0E-04, 1.0)]             
 
         return starting_point, bounds
 
@@ -55,13 +71,16 @@ class Learner(object):
         starting_point, bounds = self.initial_guess() 
         optimal = minimize(self.loss,starting_point, method='L-BFGS-B', bounds=bounds)
 
-        return tuple(optimal.x) 
+        if optimal.success == True:
+           return tuple(optimal.x) 
+        else:
+           print(optimal) 
+           return None 
 
     def loss (self, point):
-        #solution = self.E.evolve(point[0], point[1], point[2])
-        if self.args.model_name == 'sir':
+        if self.model == 'sir':
            solution = self.E.evolve(point[0], point[1])
-        if self.args.model_name == 'dbsir':
+        if self.model == 'dbsir':
            solution = self.E.evolve(point[0], point[1], point[2])
 
         y =  self.data.to_numpy()
@@ -77,27 +96,28 @@ if __name__ == "__main__":
    parser.add_argument('-i','--input-file',help='Input csv file',default='../data/covid-19-global.csv')
    parser.add_argument('-c','--country-name',help='Country Name')
    parser.add_argument('-p','--population-file',help='Country population file',default='../data/world_population.csv')
-   parser.add_argument('-m','--model-name',help='Model',default='sir')
+   parser.add_argument('-m','--model-name',help='Model',default='dbsir')
 
    args = parser.parse_args()
    df = pd.read_csv(args.input_file)
 
-   df_p = pd.read_csv(args.population_file)
-   P = df_p['pop_2020'].str.replace(",","").astype(int)
-   P.index = df_p['country'].to_list()
+   data = get_fitting_data (args, df, args.country_name,'2020-03-04')
+   days = [int(i) for i in range(0, data.shape[0])]
 
-     
+   N = get_population(args, args.country_name)
 
-
+   for index, value in data.items():
+       print(f"{index}, {value}")
 
    # Now call the Leraner  & fit 
-   L = Learner(args, P[args.country_name], df)
+   L = Learner(N, data, args.model_name)
+   L.initial_guess()
    params = L.fit()
    # Let us make prediction with the fitted parameters 
    print("best fit params:",params)
-  
+
    E = Epidemology (args.model_name,'solve_ivp',L.data.shape[0]+50)
-   E.set_init(L.N, L.I0, L.R0)
+   E.set_init(N, data[0], 0)
 
    title="Country:" + args.country_name + ", Model:" + args.model_name
    if args.model_name == 'sir':
@@ -115,8 +135,8 @@ if __name__ == "__main__":
    #plt.plot(sir.t,sir.y[0,:],c='b',label='Succeptable')
    plt.plot(sir.t,sir.y[1,:],c='r',label='Infected')
    plt.plot(sir.t,sir.y[2,:],c='g',label='Recovered')
-   plt.scatter(L.days, L.data.to_numpy(), c='b',label='Data')
-   plt.yscale('log')
+   plt.scatter(days, data.to_numpy(), c='b',label='Data')
+   #plt.yscale('log')
    plt.title(title)
    plt.legend()
    plt.savefig("plots/fit_" + args.model_name + args.country_name +".pdf")
--- models/optimizer_all.py
+++ models/optimizer_all.py
@@ -8,7 +8,8 @@ from datetime import timedelta, datetime
 import matplotlib.pyplot as plt
 from common_utils import get_country_data, date_normalize,country_normalize 
 from epidemiology import SIR, SEIR, SEIARD, Epidemology 
-from optimizer import Learner 
+from optimizer import Learner, get_fitting_data, get_population 
+
 
 def get_top_countries(df, count):
     df = country_normalize(df)
@@ -21,68 +22,60 @@ def get_top_countries(df, count):
   
     return df_top['country'].to_list()
 
+
+def show_fitting(args, N, data, country_name, params):
+
+   E = Epidemology (args.model_name,'solve_ivp',data.shape[0]+50)
+   E.set_init(N, data[0], 0)
+   days = [int(i) for i in range(0, data.shape[0])]
+
+   sir = E.evolve (params[0], params[1], params[2])
+   title = country_name  + r', $\beta_0$' +"=" + str('%.2f' %params[0])\
+      + r'$\mu$= ' +  str('%.2f' % params[1]) \
+      + r', $\gamma$= '+ str('%.2f' % params[2])
+
+   plt.plot(sir.t,sir.y[1,:],c='r',label='Infected')
+   plt.plot(sir.t,sir.y[2,:],c='g',label='Recovered')
+   plt.scatter(days, data.to_numpy(), c='b',label='Data')
+   plt.yscale('log')
+   plt.title(title)
+   plt.legend()
+   plt.savefig("plots_fit" + os.sep +  country_name + ".pdf")
+   plt.show()
+
+
 if __name__ == "__main__":
    """
    The main program 
    """
    parser = argparse.ArgumentParser()
    parser.add_argument('-i','--input-file',help='Input csv file',default='../data/covid-19-global.csv')
-   parser.add_argument('-c','--country-name',help='Country Name')
    parser.add_argument('-p','--population-file',help='Country population file',default='../data/world_population.csv')
-   parser.add_argument('-m','--model-name',help='Model',default='sir')
+   parser.add_argument('-m','--model-name',help='Model',default='dbsir')
 
    args = parser.parse_args()
    df = pd.read_csv(args.input_file)
 
-   df_p = pd.read_csv(args.population_file)
-   P = df_p['pop_2020'].str.replace(",","").astype(int)
-   P.index = df_p['country'].to_list()
-
    countries = get_top_countries(df, 50)
 
-   print("countries:",countries)
-
-   df_params = pd.DataFrame(columns=['country','beta','mu','gamma'])
+   df_params = pd.DataFrame(columns=['country','beta_0','mu','gamma'])
    count = 0
    for country in countries:
-      # Now call the Leraner  & fit 
-      print(country,  P[country])
-      L = Learner(args, P[country], df)
+      data = get_fitting_data (args, df, country,'2020-03-04')
+      days = [int(i) for i in range(0, data.shape[0])]
+      N = get_population(args, country)
+
+      L = Learner(N, data, args.model_name)
+      L.initial_guess()
       params = L.fit()
-      data = [country, "%.6f" % params[0],"%.6f" %params[1], "%.6f" % params[2]]
-      df_params.loc[count] = data
-      # Let us make prediction with the fitted parameters 
-      print(data)
-      count +=1
 
-   df_params.to_csv("params_dbsir.csv")
+      if params:
+         data_row = [country, "%.6f" % params[0],"%.6f" %params[1], "%.6f" % params[2]]
+         df_params.loc[count] = data_row 
+         # Let us make prediction with the fitted parameters 
+         print(data_row)
+         show_fitting(args, N, data, country, params)
+         count +=1
 
-   """  
-   E = Epidemology (args.model_name,'solve_ivp',L.data.shape[0]+50)
-   E.set_init(L.N, L.I0, L.R0)
-
-   title="Country:" + args.country_name + ", Model:" + args.model_name
-   if args.model_name == 'sir':
-      sir = E.evolve (params[0], params[1])
-      title = title + r', $\beta$' +"=" + str('%.2f' %params[0]) \
-         + r', $\gamma$= '+ str('%.2f' % params[1])
-
-   if args.model_name == 'dbsir':
-      sir = E.evolve (params[0], params[1], params[2])
-      title = title + r', $\beta_0$' +"=" + str('%.2f' %params[0])\
-         + r'$\mu$= ' +  str('%.2f' % params[1]) \
-         + r', $\gamma$= '+ str('%.2f' % params[2])
-
-
-   #plt.plot(sir.t,sir.y[0,:],c='b',label='Succeptable')
-   plt.plot(sir.t,sir.y[1,:],c='r',label='Infected')
-   plt.plot(sir.t,sir.y[2,:],c='g',label='Recovered')
-   plt.scatter(L.days, L.data.to_numpy(), c='b',label='Data')
-   plt.yscale('log')
-   plt.title(title)
-   plt.legend()
-   plt.savefig("plots/fit_" + args.model_name + ".pdf")
-   plt.show()
-   """
-
+   df_params.to_csv("params.csv")
 
